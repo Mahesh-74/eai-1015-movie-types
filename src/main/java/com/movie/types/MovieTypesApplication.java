809dc5be798c3f9bb33d85fbae810f363e38296c
Mahesh-74 <mahesh74.java@gmail.com>
2024-05-26 22:58:57 +0530

    FM

---
+++ src/main/java/com/movie/types/MovieTypesApplication.java
@@ -0,0 +1,13 @@
+package com.movie.types;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTypesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTypesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/movie/types/eai_1015_movie_types/Eai1015MovieTypesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movie.types.eai_1015_movie_types;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eai1015MovieTypesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
